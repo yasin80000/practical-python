9d6a2fd8e88e897e3ea166b0cbc26eb44d2f470c
abdmalique Hadge <yasin80000@hotmail.com>
2021-03-04 19:47:32 -0600

    added a little comment on what the program is doing

--- Work/bus_location.py
+++ Work/bus_location.py
@@ -1,4 +1,6 @@
 import urllib.request
+
+# chicago metro bus schedule for the next 30 minutes
 from xml.etree.ElementTree import parse
 u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14791&route=22')
 doc = parse(u)
